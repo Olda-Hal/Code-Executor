bc1110833317cd572896ae89cb0cbcd7a18551f8
Olda-Hal <78831891+Olda-Hal@users.noreply.github.com>
2025-01-13 16:11:56 +0100

    isolate transfer to bubblewrap

--- runners/python/python-runner.sh
+++ runners/python/python-runner.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-echo "$CODE" > script.py
-output=$(python script.py "$INPUT")
+
+echo "$CODE" > /var/tmp/script.py
+chmod 777 /var/tmp/script.py
+
+output=$(exec bwrap --ro-bind /usr /usr \
+    --dir /tmp \
+    --dir /var \
+    --symlink ../tmp var/tmp \
+    --proc /proc \
+    --dev /dev \
+    --ro-bind /etc/resolv.conf /etc/resolv.conf \
+    --symlink usr/lib /lib \
+    --symlink usr/lib64 /lib64 \
+    --symlink usr/bin /bin \
+    --symlink usr/sbin /sbin \
+    --chdir / \
+    --unshare-all \
+    --die-with-parent \
+    --dir /run/user/$(id -u) \
+    --setenv XDG_RUNTIME_DIR "/run/user/$(id -u)" \
+    --setenv PS1 "bwrap-demo$ " \
+    --symlink /run/user/$(id -u) /var/tmp/script.py \
+    /bin/python3 /run/user/$(id -u)/script.py)
 echo "$output"
